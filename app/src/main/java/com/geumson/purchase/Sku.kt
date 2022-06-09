510c4b4914ad508fe07db50fffd44cdcc2ec9cab
yongwon <jang_122@hanmail.net>
2022-06-09 22:28:55 +0900

    테스트 업 로드

---
+++ app/src/main/java/com/geumson/purchase/Sku.kt
@@ -0,0 +1,8 @@
+package com.geumson.purchase
+
+object Sku {
+    const val ydata_0001 = "ydata_0001"
+    const val ydata_0002 = "ydata_0002"
+    const val SUB_BASIC = "ydata_0001"
+    const val SUB_VIP = "ydata_0002"
+}
